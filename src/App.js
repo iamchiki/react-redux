81dc63b6885aa3f0325e6ea4a6f24c0057c972be
iamchiki <nikhil.soni599@gmail.com>
2022-05-24 22:07:18 +0530

    feat: implement router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Users from './pages/Users';
 
 function App() {
   return (
-    <div>
-      <Users></Users>
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path='/' element={<Navigate to='/users'></Navigate>} />
+        <Route path='users' element={<Users></Users>}></Route>
+      </Routes>
+    </React.Fragment>
   );
 }
 
