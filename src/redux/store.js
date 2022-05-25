636f34b01119c23a4c7f3ff829dec028ec12212f
iamchiki <nikhil.soni599@gmail.com>
2022-05-25 23:28:23 +0530

    remove files

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-
-const middleWares = [reduxThunk];
-
-const store = createStore(rootreducer, applyMiddleware(...middleWares));
